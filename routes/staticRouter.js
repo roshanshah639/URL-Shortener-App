67368ddfc351763891db97c164f58782850bb1f3
Roshan Shah <roshanshah639@gmail.com>
2023-12-09 11:03:32 +0545

    final commit

--- routes/staticRouter.js
+++ routes/staticRouter.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const URL = require("../models/url");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.render("home");
+router.get("/", async (req, res) => {
+  const allurls = await URL.find({});
+  res.render("home", {
+    urls: allurls,
+  });
 });
 
 module.exports = router;
